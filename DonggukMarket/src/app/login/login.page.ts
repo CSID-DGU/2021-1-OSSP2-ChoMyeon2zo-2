cb8b2f00b294cbc35319a0d4e52434bd267088cb
tommy1610 <75344562+tommy16102@users.noreply.github.com>
2021-05-11 03:24:04 +0900

    2021.05.11 / 서혜민 / 회원가입, 로그인

--- DonggukMarket/src/app/login/login.page.ts
+++ DonggukMarket/src/app/login/login.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AlertController } from '@ionic/angular';
+import firebase from 'firebase/app'
+
+export class User {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -6,10 +15,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public user:User = new User();
+  constructor(
+    public navCtrl: NavController, 
+    public fAuth: AngularFireAuth,
+    public alertController: AlertController
+  ) { }
+  async clear(){
+    this.user.email = '';
+    this.user.password = '';
+  }  
+  async login() {
+    try {
+      var r = await this.fAuth.signInWithEmailAndPassword(
+        this.user.email,
+        this.user.password
+      );
+      if(this.user.email.indexOf("@dgu.ac.kr")<0) {
+        const alert = await this.alertController.create({
+          message: '이메일 형식이 틀렸습니다!',
+          buttons: ['닫기']
+        });
+        this.clear();
+        await alert.present();
+      };
+      if(this.user.email.indexOf("@dgu.ac.kr")>=0){
+        
+        this.presentAlert();
+      }
+      
 
-  constructor() { }
+    }catch (err) {
+      const alert1 = await this.alertController.create({
 
-  ngOnInit() {
+        message: '로그인에 실패했습니다!',
+        buttons: ['닫기']
+      });
+      this.clear();
+      await alert1.present();
+      console.error(err);
+    }
   }
 
+  async presentAlert() {
+    const alert2 = await this.alertController.create({
+
+      message: '환영합니다!',
+      buttons: ['닫기']
+    });
+    
+    await alert2.present();
+    this.navCtrl.navigateRoot('');
+  }
+  ngOnInit() {
+  }
 }
