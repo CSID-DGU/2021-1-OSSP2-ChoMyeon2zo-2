7df666920f83f4ca7bc89487f9c0c158240a8475
DESKTOP-FP7CP78\gram <jiwlgus048@gmail.com>
2021-05-17 00:06:59 +0900

    2021.05.16 / 김지현 / phone-check path 삭제

--- DonggukMarket/src/app/app-routing.module.ts
+++ DonggukMarket/src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ const routes: Routes = [
   { path: 'login',loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)},
   { path: 'sign',loadChildren: () => import('./sign/sign.module').then( m => m.SignPageModule)},
   { path: 'find',loadChildren: () => import('./find/find.module').then( m => m.FindPageModule)},
-  { path: 'phone-check',loadChildren: () => import('./check/phone-check/phone-check.module').then( m => m.PhoneCheckPageModule)},
   { path: 'student-check',loadChildren: () => import('./check/student-check/student-check.module').then( m => m.StudentCheckPageModule)},
   { path: 'home',loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'alert',loadChildren: () => import('./alert/alert.module').then( m => m.AlertPageModule)},
