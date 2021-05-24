5ef641cbab78ea4751ad0a3a305146eef73e2de4
tommy1610 <tommy1610@naver.com>
2021-05-24 21:47:24 +0900

    2021.05.24 / 서혜민 / student-check 페이지 수정

--- DonggukMarket/src/app/check/student-check/student-check.page.ts
+++ DonggukMarket/src/app/check/student-check/student-check.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-check',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentCheckPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    
   }
 
+  ngOnInit() {
+    //can get id/ email from sing.page 
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+          const id = this.router.getCurrentNavigation().extras.state.id;
+          console.log(id)
+      }
+  })
+  }
+  
+  
 }
