e68db3fe1afd51a3722252932841adc916de8397
tommy1610 <tommy1610@naver.com>
2021-05-24 21:38:01 +0900

    2021/05/24/ 서혜민/ 로그인 수정

--- DonggukMarket/src/app/login/login.page.ts
+++ DonggukMarket/src/app/login/login.page.ts
@@ -4,6 +4,9 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { AlertController } from '@ionic/angular';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Storage } from '@ionic/storage';
+import * as firebase from 'firebase/app';
+
+let _this;
 export class User {
   email: string;
   password: string;
@@ -22,8 +25,7 @@ export class LoginPage implements OnInit {
     public alertController: AlertController,
     public db: AngularFireDatabase,
     public stor: Storage
-
-  ) { }
+  ) {_this=this;}
 
   async clear(){
     this.user.email = '';
@@ -36,31 +38,65 @@ export class LoginPage implements OnInit {
         this.user.email,
         this.user.password
       );
-      var temp= this.user.email.split('@');
-      if(this.user.email.indexOf("@")<0 || this.user.email.indexOf('.')<0) {
+      var id = this.user.email.split('@')[0];
+      const studentcheck = (await firebase.database().ref().child(`userinfo/${id}`).once('value')).val();
+      console.log(studentcheck.student_credit);
+      console.log('id = '+id);
+      if(studentcheck.student_credit==false) {
         const alert = await this.alertController.create({
-          message: '이메일 형식이 아닙니다!',
+          message: '학생증 인증이 완료되지 않아서 로그인이 불가합니다.',
           buttons: ['닫기']
         });
         this.clear();
         await alert.present();
-      };
-      if(this.user.email.indexOf("@")>=0 && this.user.email.indexOf(".")>=0){
-        
-        this.presentAlert();
-        this.stor.set('id',temp[0]);
       }
-      
-
+      else {
+        this.stor.set('id',id);
+        this.presentAlert();
+      }
     }catch (err) {
-      const alert1 = await this.alertController.create({
+      console.log(err.message);
+      if(err.message == 'signInWithEmailAndPassword failed: First argument "email" must be a valid string.' ||
+           err.message== 'The email address is badly formatted.'){
+        const alert1 = await this.alertController.create({
 
-        message: '로그인에 실패했습니다!',
-        buttons: ['닫기']
-      });
-      this.clear();
-      await alert1.present();
-      console.error(err);
+          message: '이메일 형식이 아닙니다.',
+          buttons: ['닫기']
+        });
+        this.clear();
+        await alert1.present();
+        console.error(err);
+      }
+      if(err.message == 'There is no user record corresponding to this identifier. The user may have been deleted.'){
+        const alert1 = await this.alertController.create({
+
+          message: '이메일이 존재하지 않습니다.',
+          buttons: ['닫기']
+        });
+        this.clear();
+        await alert1.present();
+        console.error(err);
+      }
+      if(err.message == 'The password is invalid or the user does not have a password.'){
+        const alert1 = await this.alertController.create({
+
+          message: '패스워드가 틀렸습니다.',
+          buttons: ['닫기']
+        });
+        this.clear();
+        await alert1.present();
+        console.error(err);
+      }
+      if(err.message == 'Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.'){
+        const alert1 = await this.alertController.create({
+
+          message: '비밀번호를 너무 많이 틀려 계정이 잠시 잠겼습니다.',
+          buttons: ['닫기']
+        });
+        this.clear();
+        await alert1.present();
+        console.error(err);
+      }
     }
   }
 
