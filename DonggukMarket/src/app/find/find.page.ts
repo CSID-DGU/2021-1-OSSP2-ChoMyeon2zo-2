9c1fdbe6e16e25ad0d30470f47af279da1e985fa
tommy1610 <tommy1610@naver.com>
2021-05-24 20:53:07 +0900

    2021.05.24 / 서혜민 / 이메일,비밀번호 찾기, 로그아웃

--- DonggukMarket/src/app/find/find.page.ts
+++ DonggukMarket/src/app/find/find.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AlertController } from '@ionic/angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase';
 
+let _this;
 @Component({
   selector: 'app-find',
   templateUrl: './find.page.html',
@@ -7,9 +13,141 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FindPage implements OnInit {
 
-  constructor() { }
+  email:string='';
+  name:string='';
+  
+  constructor(
+    public navCtrl: NavController, 
+    public fAuth: AngularFireAuth,
+    private alertCtrl: AlertController,
+    public db: AngularFireDatabase
+
+  ) {_this=this;}
 
   ngOnInit() {
   }
+  findemail(){
+    let info;
+    try{
+    firebase.database().ref().child('userinfo').orderByChild('name').equalTo(`${this.name}`).once('value', function(data){
+      info=data.val();
+      console.log(info);
+      if(info == null){
+        return _this.alertCtrl.create({
+          header: '',
+          message: `등록되지 않은 이름입니다`,
+          buttons: [{
+            text: '확인',
+            role: 'cancel'
+          }]
+        }).then(alertEl => {
+          alertEl.present();
+        });
+      }
+      try{var mail = info[Object.keys(info)[0]].email;}
+      catch(err){
+        console.log(err);
+      }
+      return _this.alertCtrl.create({
+        header: '',
+        message: `${_this.name} 님의 이메일은 ${mail}입니다`,
+        buttons: [{
+          text: '확인',
+          role: 'cancel'
+        }]
+      }).then(alertEl => {
+        alertEl.present();
+      });
+    });}
+    catch(err) {
+      console.log(err);
+    }
+  } 
+  resetpw(){
+    let info;
+    try{
+      firebase.database().ref().child('userinfo').orderByChild('email').equalTo(`${this.email}`).once('value', function(data){
+        info=data.val();
+        console.log(info);
+        if(info == null){
+          return _this.alertCtrl.create({
+            header: '',
+            message: `등록되지 않은 이메일입니다`,
+            buttons: [{
+              text: '확인',
+              role: 'cancel'
+            }]
+          }).then(alertEl => {
+            alertEl.present();
+          });
+        }
+        var pw = info[Object.keys(info)[0]].password;
+        var name = info[Object.keys(info)[0]].name;
+        var subpw = pw.substr(0,3);
+        var ppw = "*".repeat(pw.length);
 
+        console.log(subpw+ppw.substr(3));
+        return _this.alertCtrl.create({
+          header: '',
+          message: `${name} 님의 비밀번호는 ${subpw+ppw.substr(3)}입니다`,
+          buttons: [{
+            text: '확인',
+            role: 'cancel'
+          }]
+        }).then(alertEl => {
+          alertEl.present();
+        });
+      });}
+      catch(err) {
+        console.log(err);
+    }}
+    
+    sendMail(){
+    console.log('1차');
+    this.fAuth.auth.sendPasswordResetEmail(this.email)
+        .then((result)=>{
+          console.log(result);
+          return this.alertCtrl.create({
+            header: '',
+            message: `${this.email}로 이메일 전송 성공..!`,
+            buttons: [{
+              text: '닫기',
+              role: 'cancel'
+            }]
+          }).then(alertEl => {
+            alertEl.present();
+          });
+        }
+         
+        )
+        .catch( (error)=>{
+          var message = error.message;
+          if(message=='The email address is badly formatted.'){
+            return this.alertCtrl.create({
+              header: '',
+              message: '잘못된 이메일 형식입니다',
+              buttons: [{
+                text: '확인',
+                role: 'cancel'
+              }]
+            }).then(alertEl => {
+              alertEl.present();
+            });
+          }
+          if(message=='There is no user record corresponding to this identifier. The user may have been deleted.'){
+            return this.alertCtrl.create({
+              header: '',
+              message: '존재하지 않는 이메일입니다',
+              buttons: [{
+                text: '확인',
+                role: 'cancel'
+              }]
+            }).then(alertEl => {
+              alertEl.present();
+            });
+          }
+        });
+      
+  }
+  
 }
--- DonggukMarket/src/app/tab5/tab5.page.ts
+++ DonggukMarket/src/app/tab5/tab5.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-tab5',
@@ -7,6 +12,29 @@ import { Component } from '@angular/core';
 })
 export class Tab5Page {
 
-  constructor() {}
+  constructor(
+    public navCtrl: NavController, 
+    private alertCtrl: AlertController,
+    public fAuth: AngularFireAuth,
+    private router:Router
+  ) {}
 
+  logout(){
+    this.fAuth.auth.signOut().then((result)=>{
+      console.log(result);
+      return this.alertCtrl.create({
+        header: '',
+        message: '로그아웃에 성공하였습니다.',
+        buttons: [{
+          text: '닫기',
+          handler: () => {
+            this.router.navigateByUrl('/');
+          }
+        }]
+      }).then(alertEl => {
+        alertEl.present();
+      });
+     
+    })
+  }
 }
