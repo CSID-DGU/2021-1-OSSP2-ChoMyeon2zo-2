b8a4cdce6b0428657b65e81f1546daf59f3b8e48
tommy1610 <tommy1610@naver.com>
2021-05-26 00:02:54 +0900

    2021.05.26 / 서혜민 / 로그인 여부에 따라 마이페이지 다르게

--- DonggukMarket/src/app/tab5/tab5.page.ts
+++ DonggukMarket/src/app/tab5/tab5.page.ts
@@ -2,39 +2,104 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AlertController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
-import {Router} from '@angular/router';
+import { Storage } from '@ionic/storage';
+import * as firebase from 'firebase/app';
 
-
+let _this;
 @Component({
   selector: 'app-tab5',
   templateUrl: 'tab5.page.html',
   styleUrls: ['tab5.page.scss']
 })
 export class Tab5Page {
-
+  public userid:string="";
+  public name:string="";
+  public email:string="";
+  public major:string="";
+  public credit:string="";
+  public num:number=0;
+  public check:string="";
   constructor(
     public navCtrl: NavController, 
     private alertCtrl: AlertController,
     public fAuth: AngularFireAuth,
-    private router:Router
-  ) {}
+    public stor: Storage
+  ) {_this=this; var a;}
 
+  change(){
+    this.stor.get('id').then((val) => {
+      console.log('val = '+val);
+      this.userid = val;
+     });
+     firebase.database().ref().child(`userinfo/${this.userid}`).once('value', function(data){
+      console.log(data.val());
+      var user = data.val();
+      _this.name=user['name'];
+      _this.email=user['email'];
+      _this.major=user['major'];
+      _this.credit=user['student_credit'];
+      _this.set();
+     });
+  }
+
+  set(){
+    this.name=_this.name;
+    this.email=_this.email;
+    this.major=_this.major;
+    console.log('credit = '+_this.credit);
+    if(_this.credit==false){
+      this.check="재학생 미인증";
+    }
+    else {
+      this.check="재학생 인증";
+    }
+    console.log('set name='+_this.name+' '+this.name);
+  }
+  click(){
+    this.num++;
+    this.ngOnInit();
+  }
+  
   logout(){
     this.fAuth.auth.signOut().then((result)=>{
-      console.log(result);
+      this.stor.set('id',null);
       return this.alertCtrl.create({
         header: '',
         message: '로그아웃에 성공하였습니다.',
         buttons: [{
           text: '닫기',
           handler: () => {
-            this.router.navigateByUrl('/');
+            this.stor.get('id').then((val) => {
+              console.log('handler = '+val);
+             });
+            this.ngOnInit();
           }
         }]
       }).then(alertEl => {
         alertEl.present();
       });
-     
     })
+  }
+  ngOnInit() {
+    this.change();
+  }
+  ionViewWillEnter(){
+    this.change();
+  }
+  ionViewDidLeave(){
+    console.log('leave');
+    this.num=0;
+    this.userid="";
+    this.name="";
+    this.email="";
+    this.major="";
+    this.check="";
+
+    _this.userid="";
+    _this.name="";
+    _this.email="";
+    _this.major="";
+    _this.check="";
+    _this.userid="";
   }
 }
